56fbc4136534f38579d8c465f12fa820c3ec347d
joshuamshana <joshua.mshana@nmbtz.com>
2024-12-31 00:17:05 +0300

    Update start script

--- functions/index.mjs
+++ functions/index.mjs
@@ -10,10 +10,12 @@ export const reverseAPI = {
         const host = req?.params?._host??'';
         const path = req?.query?._path??'';
         console.log(host, '----', path);
+        req.headers.host = host;
+        // res.redirect(`https://127.0.0.1${path}`);
         proxy.web(req, res, {
             changeOrigin: true,
             target: `127.0.0.1${path}`,
-            headers: {'Host': host}
+            // headers: {'Host': host}
         }, function (e) {
             console.log(e);
             res.status(400).send(e);
