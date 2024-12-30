83b67f3497974ca241e53654984678dc9ad46834
joshuamshana <joshua.mshana@nmbtz.com>
2024-12-31 01:22:19 +0300

    Update proxy for correct mapping

--- functions/index.mjs
+++ functions/index.mjs
@@ -6,20 +6,20 @@ export const reverseAPI = {
     method: 'ALL',
     path: '/:_host',
     onRequest: (req, res) => {
-        process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
         const host = req?.params?._host??'';
         const path = req?.query?._path??'';
-        console.log(host, '----', path);
-        req.headers.host = host;
-        res.redirect(`https://127.0.0.1${path}`);
-        // proxy.web(req, res, {
-        //     changeOrigin: true,
-        //     target: `127.0.0.1${path}`,
-        //     // headers: {'Host': host}
-        // }, function (e) {
-        //     console.log(e);
-        //     res.status(400).send(e);
-        //     proxy.close();
-        // });
+        // console.log(`https://185.216.203.193${path}`);
+        proxy.web(req, res, {
+            changeOrigin: true,
+            followRedirects: true,
+            ignorePath: true,
+            target: `https://185.216.203.193${path}`,
+            secure: false,
+            headers: {'Host':host},
+        }, function (e) {
+            console.log(e);
+            res.status(400).send(e);
+            proxy.close();
+        });
     }
 }
